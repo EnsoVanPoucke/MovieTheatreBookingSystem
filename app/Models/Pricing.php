0664448d03f15cdbad9df16f4d5e02af9017a74d
Enso <159424123+EnsoVanPoucke@users.noreply.github.com>
2025-04-13 12:15:30 +0200

    Update Pricing.php

--- app/Models/Pricing.php
+++ app/Models/Pricing.php
@@ -15,13 +15,3 @@ class Pricing extends Model {
 		return $this->belongsTo(Movie::class, 'movie_id');
 	}
 }
-
-
-
-
-
-
-// protected $table = 'pricing';
-
-// protected $fillable = ['screening_date', 'screening_time', 'screen_number','tarief_single_normaal','tarief_single_korting','tarief_duo_normaal', 'tarief_duo_korting'];
-// protected $fillable = ['movie_id', 'single_seat_price', 'duo_seat_price'];
