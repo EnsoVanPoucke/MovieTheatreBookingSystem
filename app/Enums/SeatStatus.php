31574a7cdc0727111a4df823ef443ea73d390255
EnsoVanPoucke <ensovp@gmail.com>
2025-04-13 17:52:28 +0200

    update comments

--- app/Enums/SeatStatus.php
+++ app/Enums/SeatStatus.php
@@ -2,11 +2,24 @@
 
 namespace App\Enums;
 
+/*
+	Enum representing the different statuses a seat can have in the booking system.
+	Each seat is either a single or a duo seat, and can be in one of the following states:
+	- Available: The seat is free to book.
+	- Pending: The seat is currently being selected.
+	- Booked: The seat has been successfully reserved.
+*/
+
 enum SeatStatus: int {
+	// AVAILABLE seats
 	case AVAILABLE_SINGLE = 1;
 	case AVAILABLE_DUO = 1001;
+
+	// PENDING seats (during the booking process)
 	case PENDING_SINGLE = 2;
 	case PENDING_DUO = 1002;
+
+	// Booked seats
 	case BOOKED_SINGLE = 3;
 	case BOOKED_DUO = 1003;
 }
