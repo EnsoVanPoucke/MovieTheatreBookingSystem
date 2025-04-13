9f5556057c9c4aff51a24b22003577d4424d7ffd
Enso <159424123+EnsoVanPoucke@users.noreply.github.com>
2025-04-13 12:25:46 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,17 +15,4 @@ class DatabaseSeeder extends Seeder {
 			MoviesTableSeeder::class,
 		]);
 	}
-
-
-	// public function run(): void
-	// {
-	//     // User::factory(10)->create();
-
-	//     User::factory()->create([
-	//         'name' => 'Test User',
-	//         'email' => 'test@example.com',
-	//     ]);
-	// }
-
-
 }
