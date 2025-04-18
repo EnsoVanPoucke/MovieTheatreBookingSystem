35b7b2aebda79f03b5031132c3508275fdcc8d80
EnsoVanPoucke <ensovp@gmail.com>
2025-04-18 11:35:10 +0200

    updated functions MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,87 +4,69 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Movie;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 
 class MovieController extends Controller {
 
+	// get screenings, movies and banners for index page
 	public function index() {
-		$movies = collect(DB::select(
-			"SELECT m.id, 
-			m.title, 
-			m.poster_url,
-			MIN(s.screening_date) AS next_screening_date
-			FROM movies m
-			JOIN screenings s ON m.id = s.movie_id
-			WHERE s.screening_date >= CURDATE() 
-			AND s.is_public = 1
-			GROUP BY m.id, m.title, m.poster_url
-			ORDER BY next_screening_date ASC"
-		));
+		$movies = Movie::select('movies.id', 'movies.title', 'movies.poster_url')
+			->join('screenings', 'movies.id', '=', 'screenings.movie_id')
+			->whereDate('screenings.screening_date', '>=', now()->toDateString())
+			->where('screenings.is_public', 1)
+			->groupBy('movies.id', 'movies.title', 'movies.poster_url')
+			->selectRaw('MIN(screenings.screening_date) as next_screening_date')
+			->orderBy('next_screening_date', 'asc')
+			->get();
 
 		$banners = $this->getBanners();
 
 		return view('movies', compact('movies', 'banners'));
 	}
 
-	// Get movies for today
+	// Get movies for today when filtering
 	public function today() {
-		$movies = collect(DB::select(
-			"SELECT m.id, 
-            m.title, 
-            m.poster_url,
-            MIN(s.screening_date) AS next_screening_date
-            FROM movies m
-            JOIN screenings s ON m.id = s.movie_id
-            WHERE s.screening_date = CURDATE() 
-            AND s.is_public = 1
-            GROUP BY m.id, m.title, m.poster_url
-            ORDER BY next_screening_date ASC"
-		));
+		$movies = Movie::select('movies.id', 'movies.title', 'movies.poster_url')
+			->join('screenings', 'movies.id', '=', 'screenings.movie_id')
+			->whereDate('screenings.screening_date', now()->toDateString())
+			->where('screenings.is_public', 1)
+			->groupBy('movies.id', 'movies.title', 'movies.poster_url')
+			->selectRaw('MIN(screenings.screening_date) as next_screening_date')
+			->orderBy('next_screening_date', 'asc')
+			->get();
 
 		return view('movies', compact('movies'));
 	}
 
-	// Get movies for tomorrow
+	// Get movies for tomorrow when filtering
 	public function tomorrow() {
-		$movies = collect(DB::select(
-			"SELECT m.id, 
-            m.title, 
-            m.poster_url,
-            MIN(s.screening_date) AS next_screening_date
-            FROM movies m
-            JOIN screenings s ON m.id = s.movie_id
-            WHERE s.screening_date = CURDATE() + INTERVAL 1 DAY
-            AND s.is_public = 1
-            GROUP BY m.id, m.title, m.poster_url
-            ORDER BY next_screening_date ASC"
-		));
+		$movies = \App\Models\Movie::select('movies.id', 'movies.title', 'movies.poster_url')
+			->join('screenings', 'movies.id', '=', 'screenings.movie_id')
+			->whereDate('screenings.screening_date', now()->addDay()->toDateString())
+			->where('screenings.is_public', 1)
+			->groupBy('movies.id', 'movies.title', 'movies.poster_url')
+			->selectRaw('MIN(screenings.screening_date) as next_screening_date')
+			->orderBy('next_screening_date', 'asc')
+			->get();
 
 		return view('movies', compact('movies'));
 	}
 
+	// get selected movie by id
 	public function show($id) {
 		// Fetch movie details by id
-		$movieData = DB::selectOne(
-			"SELECT * FROM movies
-			WHERE id = :id",
-			['id' => $id]
-		);
+		$movieData = \App\Models\Movie::findOrFail($id);
 
 		// Fetch associated screenings for this movie (for dropdown menu with dates)
-		$movieScreenings = collect(DB::select(
-			"SELECT * FROM screenings 
-        	WHERE movie_id = :id
-        	AND is_public = 1 
-        	ORDER BY screening_date ASC",
-			['id' => $id]
-		));
+		$movieScreenings = $movieData->screenings()
+			->where('is_public', 1)
+			->orderBy('screening_date', 'asc')
+			->get();
 
 		return view('movie_detail', compact('movieData', 'movieScreenings'));
 	}
 
+	// get banners
 	private function getBanners() {
 		$bannersPath = public_path('images/banners');
 		$banners = [];
@@ -95,82 +77,6 @@ class MovieController extends Controller {
 				$banners[] = 'images/banners/' . $file->getFilename();
 			}
 		}
-
 		return $banners;
 	}
 }
-
-
-
-	// // get screenings
-	// public function index() {
-	// 	$movies = Movie::whereHas('screenings', function ($query) {
-	// 		$query->whereDate('screening_date', '>=', Carbon::today())
-	// 			->where('is_public', true);
-	// 	})
-	// 		->with(['screenings' => function ($query) {
-	// 			$query->orderBy('screening_date'); // Order screenings properly
-	// 		}])
-	// 		->get();
-
-	// 	return view('movies', compact('movies'));
-	// }
-
-	// // get selected movie by id.
-	// public function show(Movie $movie) {
-	// 	$screenings = $movie->screenings()
-	// 		->where('is_public', true)
-	// 		->orderBy('screening_date')
-	// 		->get();
-
-	// 	return view('movie_detail', compact('movie', 'screenings'));
-	// }
-
-
-// dd($movie->toArray());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// public function index() {
-// 	$movies = Movie::all();
-// 	return view('movies', compact('movies'));
-// }
-
-// public function show($id) {
-// 	$movie = Movie::where('id', $id)->firstOrFail();
-
-// 	$screenings = $movie->screenings()
-// 		->where('screening_date', '>=', Carbon::now()) // Get only future screenings
-// 		->orderBy('screening_date')
-// 		->orderBy('screening_time')
-// 		->get();
-
-// 	// Group screenings by date
-// 	$groupedScreenings = $screenings->groupBy('screening_date');
-
-// 	return view('movie_info', compact('movie', 'groupedScreenings'));
-// }
